8b031ab41bdfcd8df284b5386a732991a044d08a
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-14 16:08:37 +0530

    create hash table

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HashTable
+{
+    class Program
+    {
+        /// <summary>
+        /// --------------------->Welcome To Hash Table<-----------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---------------->Welcome To Hash Table<-----------------");
+
+
+            MyMapNode<int, string> hashTable = new MyMapNode<int, string>(6);
+            hashTable.Add(0, "To");
+            hashTable.Add(1, "be");
+            hashTable.Add(2, "or");
+            hashTable.Add(3, "not");
+            hashTable.Add(4, "to");
+            hashTable.Add(5, "be");
+
+        }
+    }
+}
